d07b665305f19fb20d33ab7df53cc7f7ebdd76b1
RootCellar <RootCellar9877@GMail.com>
2022-03-29 16:52:08 -0800

    complete findList

--- assignment5/PA5.hs
+++ assignment5/PA5.hs
@@ -7,25 +7,30 @@
 
 module PA5 where
 
+import Data.List
 
 -- =====================================================================
 
-collatz n
+collatzC n
   | n == 1 = 0
-  | mod n 2 == 0 = collatz (div n 2) + 1
-  | otherwise = collatz (3*n+1) + 1
+  | mod n 2 == 0 = collatzC (div n 2) + 1 -- even
+  | otherwise = collatzC (3*n+1) + 1 -- odd
 
 -- collatzCounts
 collatzCounts :: [Integer]
-collatzCounts = map collatz [1..]  -- DUMMY; REWRITE THIS!!!
+collatzCounts = map collatzC [1..]
 
 
 -- =====================================================================
 
+findL n pre list
+    | length list == 0 = Nothing -- Nothing left
+    | isPrefixOf pre list == False = findL (n+1) pre (drop 1 list) -- Continuing search
+    | isPrefixOf pre list == True = Just n -- Found it!
 
 -- findList
 findList :: Eq a => [a] -> [a] -> Maybe Int
-findList _ _ = Just 42  -- DUMMY; REWRITE THIS!!!
+findList pre list = findL 0 pre list
 
 
 -- =====================================================================
