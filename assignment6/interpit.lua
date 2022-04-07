7f6e690f376a760672c565844d34c384c7087424
RootCellar <RootCellar9877@GMail.com>
2022-04-07 11:18:16 -0800

    get all ifs working, work on while loops

--- assignment6/interpit.lua
+++ assignment6/interpit.lua
@@ -277,19 +277,26 @@ function interpit.interp(ast, state, incall, outcall)
             for spot = 2, #ast, 2 do
               -- Plan: loop in increments of 2
               local condition = ast[spot]
-              if condition[1] == SMT_LIST then
+              print(astToStr(condition))
+              if condition[1] == STMT_LIST then
                   -- This isn't a condition, it's an else!
                   if condition[2] ~= nil then
                     interp_stmt(condition[2])
                   end
-                  break
+                  return
               end
               local body = ast[spot+1]
               local result = eval_expr(condition)
               if result ~= 0 and body[2] ~= nil then
                   interp_stmt(body[2])
-                  break
+                  return
               end
+            end
+        elseif ast[1] == WHILE_LOOP then
+            local condition = ast[2]
+            local body = ast[3]
+            while eval_expr(condition) ~= 0 and body[2] ~= nil do
+                interp_stmt(body[2])
             end
         else
             print("*** UNIMPLEMENTED STATEMENT")
