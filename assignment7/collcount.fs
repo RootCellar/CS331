b0d6d5a05f349e10b8b914bb43d3aa98aec98507
RootCellar <RootCellar9877@GMail.com>
2022-04-25 08:54:11 -0800

    re-arrange, collatz-step must be defined first

--- assignment7/collcount.fs
+++ assignment7/collcount.fs
@@ -2,6 +2,16 @@
 \ 4/25/2022
 \ Writing collcount word for Assignment 7
 
+\ From Glenn G. Chappell's "word.fs"
+\ https://github.com/ggchappell/cs331-2022-01/blob/main/word.fs
+: collatz-step  { n -- number_after_n_in_Collatz_sequence }
+  n 2 mod 0 = if  \ Is n even?
+    n 2 /
+  else
+    n 3 * 1 +
+  endif
+;
+
 : collcount { n -- c }
   0 { c }
 
@@ -11,15 +21,4 @@
     c 1 + to c
   repeat
   c
-;
-
-
-\ From Glenn G. Chappell's "word.fs"
-\ https://github.com/ggchappell/cs331-2022-01/blob/main/word.fs
-: collatz-step  { n -- number_after_n_in_Collatz_sequence }
-  n 2 mod 0 = if  \ Is n even?
-    n 2 /
-  else
-    n 3 * 1 +
-  endif
 ;
