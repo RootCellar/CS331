591f57f4f89a7e9f965d91bc58d1f9be5168b266
RootCellar <RootCellar9877@GMail.com>
2022-04-26 15:11:00 -0800

    remove addpair, use append and list procedures instead

--- assignment7/addpairs.scm
+++ assignment7/addpairs.scm
@@ -2,18 +2,10 @@
 ; 4/25/2022
 ; Writing a procedure in Scheme for Assignment 7
 
-(define (addpair . p)
-  (cond
-    ((null? p) 0)
-    ((null? cadr p) 0)
-    (else (+ (car p) (cadr p)))
-  )
-)
-
 (define (addpairs . args)
-(cond
-  ((null? args) 0)
-  ((not (pair? args)) (error "add: args do not form a list"))
-  (else (cons (addpair (car args) (cadr args)) (apply addpairs (cdr (cdr args)))))
+  (cond
+    ((null? args) ())
+    ((= 1 (length args)) args)
+    (else (append (list (+ (car args) (cadr args))) (apply addpairs (cdr (cdr args)))))
   )
 )
