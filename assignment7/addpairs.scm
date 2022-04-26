6100de81f64826f06cbd23c1b28b14a0231246ba
RootCellar <RootCellar9877@GMail.com>
2022-04-25 19:36:21 -0800

    scheme program nearly correct

--- assignment7/addpairs.scm
+++ assignment7/addpairs.scm
@@ -1,7 +1,15 @@
-(define (addpair p)
-  (list (+ (car p) (car (cadr p)) ) )
+; Darian Marvel
+; 4/25/2022
+; Writing a procedure in Scheme for Assignment 7
+
+(define (addpair . p)
+  (+ (car p) (cadr p))
 )
 
-(define (addpairs (list l))
-  (car (addpair l))
+(define (addpairs . args)
+(cond
+  ((null? args) 0)
+  ((not (pair? args)) (error "add: args do not form a list"))
+  (else (cons (addpair (car args) (cadr args)) (apply addpairs (cdr (cdr args)))))
+  )
 )
