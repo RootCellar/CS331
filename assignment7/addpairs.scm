e5d03a79f46eef0efe9141505d797eee61839082
RootCellar <RootCellar9877@GMail.com>
2022-04-26 19:32:26 -0800

    comments, formatting

--- assignment7/addpairs.scm
+++ assignment7/addpairs.scm
@@ -1,4 +1,4 @@
-#lang scheme
+;#lang scheme
 
 ; Darian Marvel
 ; 4/25/2022
@@ -6,8 +6,13 @@
 
 (define (addpairs . args)
   (cond
+    ; handle even number of arguments (or none...)
     ((null? args) ())
+
+    ; handle odd number of arguments
     ((= (length args) 1) args)
+
+    ; otherwise recursively append results to a list
     (else (append (list (+ (car args) (cadr args))) (apply addpairs (cdr (cdr args)))))
   )
 )
